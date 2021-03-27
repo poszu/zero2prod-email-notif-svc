6c823098377cd7c2907dda810bb12a7f61d1e70e
Bartosz Różański <bartek.roza@gmail.com>
2021-03-27 22:12:31 +0100

    Finished chapter 4 - observability

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
