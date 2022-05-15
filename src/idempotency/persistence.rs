df9db6e32455d387733c54352e1b4d43d092ede9
Bartosz Różański <bartek.roza@gmail.com>
2022-05-15 20:15:29 +0200

    Handle concurrent newsletter publishing

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -2,7 +2,7 @@ use std::convert::TryInto;
 
 use actix_web::{body::to_bytes, HttpResponse};
 use reqwest::StatusCode;
-use sqlx::{postgres::PgHasArrayType, PgPool};
+use sqlx::{postgres::PgHasArrayType, PgPool, Postgres, Transaction};
 use uuid::Uuid;
 
 use super::IdempotencyKey;
@@ -20,6 +20,44 @@ impl PgHasArrayType for HeaderPairRecord {
     }
 }
 
+pub enum NextAction {
+    StartProcessing(Transaction<'static, Postgres>),
+    ReturnSavedReponse(HttpResponse),
+}
+
+pub async fn try_processing(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+) -> Result<NextAction, anyhow::Error> {
+    let mut transaction = pool.begin().await?;
+    let n_inserted_rows = sqlx::query!(
+        r#"
+        INSERT INTO idempotency (
+            user_id,
+            idempotency_key,
+            created_at
+        )
+        VALUES ($1, $2, now())
+        ON CONFLICT DO NOTHING
+        "#,
+        user_id,
+        idempotency_key.as_ref()
+    )
+    .execute(&mut transaction)
+    .await?
+    .rows_affected();
+
+    if n_inserted_rows > 0 {
+        Ok(NextAction::StartProcessing(transaction))
+    } else {
+        let saved_response = get_saved_response(pool, idempotency_key, user_id)
+            .await?
+            .ok_or_else(|| anyhow::anyhow!("We expected a saved response but didn't find it"))?;
+        Ok(NextAction::ReturnSavedReponse(saved_response))
+    }
+}
+
 pub async fn get_saved_response(
     pool: &PgPool,
     idempotency_key: &IdempotencyKey,
@@ -28,9 +66,9 @@ pub async fn get_saved_response(
     let saved_response = sqlx::query!(
         r#"
         SELECT
-            response_status_code,
-            response_headers as "response_headers: Vec<HeaderPairRecord>",
-            response_body
+            response_status_code as "response_status_code!",
+            response_headers as "response_headers!: Vec<HeaderPairRecord>",
+            response_body as "response_body!"
         FROM idempotency
         WHERE user_id = $1 AND idempotency_key = $2"#,
         user_id,
@@ -52,7 +90,7 @@ pub async fn get_saved_response(
 }
 
 pub async fn save_response(
-    pool: &PgPool,
+    mut transaction: Transaction<'static, Postgres>,
     idempotency_key: &IdempotencyKey,
     user_id: Uuid,
     http_response: HttpResponse,
@@ -72,15 +110,24 @@ pub async fn save_response(
 
     sqlx::query_unchecked!(
         r#"
-        INSERT INTO idempotency (user_id, idempotency_key, response_status_code, response_headers, response_body, created_at)
-        VALUES ($1, $2, $3, $4, $5, now())
+        UPDATE idempotency
+        SET
+            response_status_code = $3,
+            response_headers = $4,
+            response_body = $5
+        WHERE
+            user_id = $1 AND
+            idempotency_key = $2
         "#,
         user_id,
         idempotency_key.as_ref(),
         status_code,
         headers,
         body.as_ref()
-    ).execute(pool).await?;
+    )
+    .execute(&mut transaction)
+    .await?;
+    transaction.commit().await?;
 
     let http_response = response_head.set_body(body).map_into_boxed_body();
     Ok(http_response)
--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -2,7 +2,9 @@ use std::convert::TryInto;
 
 use crate::authentication::UserId;
 use crate::email_client::EmailClient;
-use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
+use crate::idempotency::{
+    get_saved_response, save_response, try_processing, IdempotencyKey, NextAction,
+};
 use crate::utils::{e400, e500};
 use crate::{domain::SubscriberEmail, utils::see_other};
 use actix_web::{web, HttpResponse};
@@ -55,6 +57,18 @@ pub async fn publish_newsletter(
         idempotency_key,
     } = form.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+
+    let transaction = match try_processing(&pool, &idempotency_key, *user_id)
+        .await
+        .map_err(e500)?
+    {
+        NextAction::StartProcessing(t) => t,
+        NextAction::ReturnSavedReponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
+        }
+    };
+
     if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
@@ -84,13 +98,18 @@ pub async fn publish_newsletter(
             }
         }
     }
-    FlashMessage::info("The newsletter issue has been published!").send();
+
+    success_message().send();
     save_response(
-        &pool,
+        transaction,
         &idempotency_key,
         *user_id,
         see_other("/admin/newsletters"),
     )
     .await
     .map_err(e500)
+}
+
+fn success_message() -> FlashMessage {
+    FlashMessage::info("The newsletter issue has been published!")
 }
